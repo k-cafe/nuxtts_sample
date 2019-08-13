2d1c607c3ab6d53f2cb38a8e618808207b608008
K-Coffee <hekouki111@gmail.com>
2019-08-13 23:02:28 +0900

    add injector type

--- plugins/firebase-injector.ts
+++ plugins/firebase-injector.ts
@@ -1,12 +1,13 @@
-import * as firebase from 'firebase/app'
+import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import { Context } from '@nuxt/vue-app'
 import { firebaseConfig } from '~/environments/firebase'
-
+import { VueInject } from '~/typealias';
 
 const fb = firebase.initializeApp(firebaseConfig)
 
-export default (context: any, inject: any) => {
+export default (context: Context, inject: VueInject) => {
   inject('firebase', fb)
   inject('firestore', fb.firestore())
   inject('auth', fb.auth())
--- typealias/index.ts
+++ typealias/index.ts
@@ -1,1 +1,2 @@
 export type MappingObject<T> = { UNKNOWN: T, [key: string]: T }
+export type VueInject = (targetKey: string, value: any) => void
