4f344f73ca9771efb529c404fba0ea0a046ba69e
K-Coffee <hekouki111@gmail.com>
2019-08-19 23:16:04 +0900

    show Splash in 300ms when loading at first

--- components/presenters/Splash.vue
+++ components/presenters/Splash.vue
@@ -14,23 +14,52 @@
 </template>
 
 <script lang="ts">
+import { setTimeout } from 'timers'
 import { Component, Vue } from 'nuxt-property-decorator'
 import { LifecycleHook } from '../../interfaces/lifecycle-hook.interface'
 import { Nullable } from '../../typealias'
 import { VuexExtention } from '../../types'
+import { CanMove } from '../../interfaces/can-move.interface'
 import { commandTypes as AuthCommand } from '~/store/auth'
 
 @Component
-export default class SplashComponent extends Vue implements LifecycleHook {
+export default class SplashComponent extends Vue
+  implements LifecycleHook, CanMove {
   private loading = true
   private unsubscribe: Nullable<VuexExtention.Unsubscriber> = null
 
-  created() {
-    this.unsubscribe = this.$store.subscribe((mutation) => {
-      if (mutation.type !== `auth/${AuthCommand.mutationTypes.SET_SIGNIN_USER}`)
-        return
-      this.loading = false
+  private isNoSignedInUser(mutationType: string): boolean {
+    const setSignInUser = `auth/${AuthCommand.mutationTypes.SET_SIGNIN_USER}`
+    return mutationType !== setSignInUser
+  }
+
+  private get isAuthorized() {
+    const isAuthorized = `auth/${AuthCommand.getterTypes.IS_AUTHORIZED}`
+    return this.$store.getters[isAuthorized]
+  }
+
+  moveTo() {
+    this.$nuxt.$router.replace({ name: 'login' }).then(() => {
+      // Pageの描画処理を待つ
+      setTimeout(() => {
+        this.loading = false
+      }, 300)
     })
+  }
+
+  private showCurrentPageIfAuthorized() {
+    return this.$store.subscribe((mutation) => {
+      if (this.isNoSignedInUser(mutation.type)) return
+      if (!this.isAuthorized) {
+        this.moveTo()
+      } else {
+        this.loading = false
+      }
+    })
+  }
+
+  created() {
+    this.unsubscribe = this.showCurrentPageIfAuthorized()
   }
 
   beforeDestroy() {
