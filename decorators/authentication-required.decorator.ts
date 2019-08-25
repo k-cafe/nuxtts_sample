8850c7dff85037f57e4373070281a2aea2f60071
K-Coffee <hekouki111@gmail.com>
2019-08-25 11:06:44 +0900

    fix

--- decorators/authentication-required.decorator.ts
+++ decorators/authentication-required.decorator.ts
@@ -15,7 +15,9 @@ export const authenticationRequired = <T extends ClassObject>(
     fetch(context: Context) {
       callSuperFetchIfDefined(context, super.fetch)
 
-      if (isFirstAccessed(context) || isAuthorized(context)) return
+      if (isFirstAccessed(context)) return
+
+      if (isAuthorized(context)) return
 
       redirectLoginPage(context)
     }
